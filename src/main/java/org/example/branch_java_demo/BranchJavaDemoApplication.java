b03969ea208abe8363c414d447367a12745c9703
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-04-25 12:19:01 +0700

    init

---
+++ src/main/java/org/example/branch_java_demo/BranchJavaDemoApplication.java
@@ -0,0 +1,14 @@
+package org.example.branch_java_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchJavaDemoApplication.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/org/example/branch_java_demo/BranchJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.branch_java_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchJavaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
