c8cdc4a4735ab446599cfdb0f3f9dbd680619f0c
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-04-25 13:23:33 +0700

    Setup CustomMethods

---
+++ src/main/java/org/example/branch_java_demo/utils/CustomMethods.java
@@ -0,0 +1,4 @@
+package org.example.branch_java_demo.utils;
+
+public class CustomMethods {
+}
---
+++ src/main/java/org/example/branch_java_demo/utils/Main.java
@@ -0,0 +1,9 @@
+package org.example.branch_java_demo.utils;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Oki");
+    }
+}
