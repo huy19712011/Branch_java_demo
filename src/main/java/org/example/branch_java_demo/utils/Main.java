c685f803a77637ac88249a36e7aacb2feb361ef8
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-04-25 13:40:08 +0700

    sub_method

--- src/main/java/org/example/branch_java_demo/utils/Main.java
+++ src/main/java/org/example/branch_java_demo/utils/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Oki");
+
+        CustomMethods methods = new CustomMethods();
+
+        System.out.println(methods.sub(5, 2));
     }
 }
