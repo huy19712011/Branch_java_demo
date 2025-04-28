4416fdb40c9de4ae6dcc770f134f1d9c669a3574
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-04-28 16:55:14 +0700

    Changed int to double

--- src/main/java/org/example/branch_java_demo/utils/CustomMethods.java
+++ src/main/java/org/example/branch_java_demo/utils/CustomMethods.java
@@ -2,7 +2,7 @@ package org.example.branch_java_demo.utils;
 
 public class CustomMethods {
 
-    public int add(int x, int y) {
+    public double add(double x, double y) {
 
         return x + y;
     }
